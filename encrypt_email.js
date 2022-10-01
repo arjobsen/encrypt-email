d0e495a451143b378c3afcd325835a55a2117452
Arjen <arjen@bos4.nl>
2022-10-01 22:12:50 +0200

    Initial commit

---
+++ encrypt_email.js
@@ -0,0 +1,20 @@
+// encrypt_email.js
+
+function stringToCharCodes(string) {
+    // Returns a list of the character codes of a string
+    return [...string].map(c => c.charCodeAt(0));
+}
+
+function encrypt(string) {
+    // String to character codes, encrypt (+1) and back to a string
+    let charCodes = stringToCharCodes(string);
+    let encrCharCodes = charCodes.map(e => e += 1);
+    return String.fromCharCode(...encrCharCodes);
+}
+
+function decrypt(encrString) {
+    // String to character codes, decrypt (-1) and back to a string
+    let encrCharCodes = stringToCharCodes(encrString);
+    let decrCharCodes = encrCharCodes.map(e => e -= 1);
+    return String.fromCharCode(...decrCharCodes);
+}
